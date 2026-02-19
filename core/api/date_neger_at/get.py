a93739887d385b594188cb234d81ea744b6629d5
1ChaLLengeR1 <artek.scibor@gmail.com>
2026-02-19 19:48:47 +0100

    - add requests to requirements.txt

--- core/api/date_neger_at/get.py
+++ core/api/date_neger_at/get.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from core.data.api.date_nager_at.get import Holiday
+import requests
 
 
 def fetch_date_nager_at_pl(year: int, country: str) -> tuple[Optional[list[Holiday]], Optional[str], bool]:
     try:
-        import requests
+
         response = requests.get(f"https://date.nager.at/api/v3/PublicHolidays/{year}/{country.upper()}", timeout=10)
         response.raise_for_status()
 
